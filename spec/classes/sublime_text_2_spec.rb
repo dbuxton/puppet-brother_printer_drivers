c712fd79ee0892c0a66d648ccf8aef520e7b94ad
David Buxton <david@arachnys.com>
2013-07-19 14:08:29 +0100

    Plural

--- spec/classes/sublime_text_2_spec.rb
+++ spec/classes/sublime_text_2_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe 'brother_printer_driver' do
+describe 'brother_printer_drivers' do
   it do
-    should contain_package('brother_printer_driver').with({
+    should contain_package('brother_printer_drivers').with({
       :provider => 'appdmg',
       :source   => 'https://www.dropbox.com/s/t4sdo43sbqng9op/brxclaserX6_410.dmg'
     })
